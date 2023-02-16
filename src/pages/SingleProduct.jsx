3399d30838db384223981e327b798d6dbf49c0f8
JSpapi <104095661+JSpapi@users.noreply.github.com>
2023-02-16 22:37:48 +0500

    finished-project

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -5,18 +5,18 @@ export const SingleProduct = () => {
   const { productId } = useParams();
   const { id, img, name, desc, price } = data.find((p) => p.id === +productId);
   return (
-    <section className="sectiont-title">
+    <section className="section-padding singleProduct">
       <div className="container">
         <div className="singleProduct__content">
           <div className="singleProduct__content-left">
-            <img src="" alt="" />
+            <img src={img} alt={name} />
           </div>
           <div className="singleProduct__content-right">
-            <h1></h1>
-            <p></p>
-            <p></p>
+            <h1 className="singleProduct__content-title">{name}</h1>
+            <p className="singleProduct__content-price">${price} USD</p>
+            <p className="singleProduct__content-info">{desc}</p>
 
-            <NavLink to="/products" className="btn">
+            <NavLink to="/products" className="btn" style={{background: '#942e2e'}}>
               Back to products
             </NavLink>
           </div>
